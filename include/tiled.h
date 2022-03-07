539e797a11b13e3c71c1ff3c525ab209b9c32152
Yesh <yesh@aliyun.com>
2022-03-07 18:30:58 +0800

    Added TiledMap (.tmx, .tsx files from Tiled)

---
+++ include/tiled.h
@@ -0,0 +1,62 @@
+#ifndef DOWNPOUR_TILED_H
+#define DOWNPOUR_TILED_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "SFML/Graphics.hpp"
+#include "pugixml.hpp"
+
+#include "asset_manager.h"
+
+class TiledMap;
+
+struct TiledSet {
+  const std::string filename;
+  const size_t tileWidth, tileHeight;
+  const size_t columns, rows;
+};
+
+class TiledLoader {
+public:
+  typedef std::unordered_map<std::string, sf::Texture> TextureMap;
+  typedef std::map<std::string, TiledSet> TileSetMap;
+  typedef std::map<size_t, TiledSet, std::greater<size_t>> UsedSets;
+
+private:
+  std::unordered_map<std::string, sf::Texture> textures;
+  std::map<std::string, TiledSet> tileSets;
+  AssetManager &assets;
+  void addTileSet(const std::string &name, size_t first, UsedSets &used);
+
+public:
+  TiledLoader(AssetManager &manager);
+  TiledMap load(std::string filename);
+};
+
+class TiledLayer : public sf::Drawable, public sf::Transformable {
+private:
+  std::string name;
+  std::vector<std::vector<sf::Vertex>> vertices;
+  std::vector<const sf::Texture *> textures;
+  void draw(sf::RenderTarget &target, const sf::RenderStates &states) const;
+
+public:
+  TiledLayer(const std::string &name, size_t width, size_t height,
+             size_t tileWidth, size_t tileHeight,
+             const std::vector<size_t> &tiles,
+             const TiledLoader::UsedSets &usedSets,
+             const TiledLoader::TextureMap &textureMap);
+};
+
+class TiledMap : public sf::Drawable, public sf::Transformable {
+  friend class TiledLoader;
+private:
+  std::vector<TiledLayer> layers;
+  TiledMap() = default;
+  TiledMap(std::vector<TiledLayer> &&layers);
+  void draw(sf::RenderTarget &target, const sf::RenderStates &states) const;
+};
+
+#endif /* !DOWNPOUR_TILED_H */
---
+++ src/tiled.cc
@@ -0,0 +1,190 @@
+#include <map>
+#include <string>
+#include <vector>
+
+#include "pugixml.hpp"
+
+#include "tiled.h"
+
+using namespace pugi;
+using namespace std;
+using namespace sf;
+
+static const TiledLoader::UsedSets::const_iterator
+getTiledSetFromTile(size_t tileId, const TiledLoader::UsedSets &usedSets) {
+  auto i = usedSets.lower_bound(tileId);
+  if (i == usedSets.end()) {
+    throw runtime_error("Probably data corruption");
+  }
+  return i;
+}
+
+TiledLayer::TiledLayer(const std::string &name, size_t width, size_t height,
+                       size_t tileWidth, size_t tileHeight,
+                       const std::vector<size_t> &tiles,
+                       const TiledLoader::UsedSets &usedSets,
+                       const TiledLoader::TextureMap &textureMap) {
+  int i = 0;
+  size_t textureCount = 0;
+  map<const string, size_t> textureIndices;
+  for (int y = 0; y != height; ++y) {
+    for (int x = 0; x != width; ++x, ++i) {
+      size_t tileId = tiles[i];
+      if (tileId == 0) {
+        continue;
+      }
+      /* Get tile info */
+      auto tiledSetPair = getTiledSetFromTile(tileId, usedSets);
+      auto &tiledSet = tiledSetPair->second;
+      size_t tileOffset = tileId - tiledSetPair->first;
+      size_t tileSetX = tileOffset % tiledSet.columns;
+      size_t tileSetY = tileOffset / tiledSet.columns;
+
+      /* Get corresponding tileSet */
+      auto iter = textureIndices.find(tiledSet.filename);
+      size_t index;
+      if (iter == textureIndices.end()) {
+        textureIndices.insert(make_pair(tiledSet.filename, textureCount));
+        index = textureCount;
+        ++textureCount;
+        textures.push_back(&textureMap.at(tiledSet.filename));
+        vertices.emplace_back();
+      } else {
+        index = iter->second;
+      }
+
+      /* Compute vertices */
+      auto &verts = vertices[index];
+      size_t textureW = tiledSet.tileWidth, textureH = tiledSet.tileHeight;
+      size_t textureX = textureW * tileSetX, textureY = textureH * tileSetY;
+      size_t tileX = tileWidth * x, tileY = tileHeight * y;
+      verts.push_back(
+          Vertex(Vector2f(tileX, tileY), Vector2f(textureX, textureY)));
+      verts.push_back(Vertex(Vector2f(tileX + tileWidth, tileY),
+                             Vector2f(textureX + textureW, textureY)));
+      verts.push_back(Vertex(Vector2f(tileX, tileY + tileHeight),
+                             Vector2f(textureX, textureY + textureH)));
+      verts.push_back(Vertex(Vector2f(tileX + tileWidth, tileY),
+                             Vector2f(textureX + textureW, textureY)));
+      verts.push_back(Vertex(Vector2f(tileX, tileY + tileHeight),
+                             Vector2f(textureX, textureY + textureH)));
+      verts.push_back(
+          Vertex(Vector2f(tileX + tileWidth, tileY + tileHeight),
+                 Vector2f(textureX + textureW, textureY + textureH)));
+    }
+  }
+}
+
+void TiledLayer::draw(sf::RenderTarget &target,
+                      const sf::RenderStates &states) const {
+  RenderStates mine(states);
+  mine.transform *= getTransform();
+  for (int i = 0; i != textures.size(); ++i) {
+    auto &verts = vertices[i];
+    auto texture = textures[i];
+    mine.texture = texture;
+    target.draw(verts.data(), verts.size(), sf::Triangles, mine);
+  }
+}
+
+TiledLoader::TiledLoader(AssetManager &manager) : assets(manager) {}
+
+static TiledSet loadFromTSX(string filename, AssetManager &assets) {
+  auto file = assets.getData(filename);
+  xml_document tsx;
+  tsx.load_buffer(file->data, file->size);
+  auto tilesetNode = tsx.child("tileset");
+  size_t tileWidth = stol(tilesetNode.attribute("tilewidth").value());
+  size_t tileHeight = stol(tilesetNode.attribute("tileheight").value());
+  size_t columns = stol(tilesetNode.attribute("columns").value());
+  size_t rows = stol(tilesetNode.attribute("tilecount").value()) / columns;
+  return TiledSet{tilesetNode.child("image").attribute("source").value(),
+                  tileWidth, tileHeight, columns, rows};
+}
+
+static void parseTileData(const string &data, vector<size_t> &tiles) {
+  tiles.clear();
+  for (size_t i = 0; i < data.size(); ++i) {
+    try {
+      size_t offset;
+      size_t tile = stol(data.substr(i), &offset);
+      tiles.push_back(tile);
+      i += offset;
+    } catch (...) {
+    }
+  }
+}
+
+TiledMap TiledLoader::load(std::string filename) {
+  auto file = assets.getData(filename);
+  xml_document tmx;
+  tmx.load_buffer(file->data, file->size);
+
+  auto mapNode = tmx.child("map");
+  size_t columns, rows;
+  if (string("left-down") == mapNode.attribute("renderorder").value()) {
+    /* Map info */
+    size_t tileWidth = stol(mapNode.attribute("tilewidth").value());
+    size_t tileHeight = stol(mapNode.attribute("tileheight").value());
+
+    /* Gather tilesets */
+    UsedSets usedSets;
+    for (auto node : mapNode.children("tileset")) {
+      auto tilesetFile = string(node.attribute("source").value());
+      addTileSet(tilesetFile, stol(node.attribute("firstgid").value()),
+                 usedSets);
+    }
+
+    columns = stoi(mapNode.attribute("width").value());
+    rows = stoi(mapNode.attribute("height").value());
+    /* Extract layers */
+    vector<TiledLayer> layers;
+    for (auto node : mapNode.children("layer")) {
+      size_t id = stol(node.attribute("id").value());
+      string name(node.attribute("name").value());
+      size_t width = stol(node.attribute("width").value());
+      size_t height = stol(node.attribute("height").value());
+      auto dataNode = node.child("data");
+      if (string("csv") == dataNode.attribute("encoding").value()) {
+        auto data = dataNode.child_value();
+        vector<size_t> tiles;
+        tiles.reserve(width * height);
+        parseTileData(data, tiles);
+        layers.emplace_back(name, width, height, tileWidth, tileHeight, tiles,
+                            usedSets, textures);
+      }
+    }
+    return TiledMap(std::move(layers));
+  } else {
+    return TiledMap{};
+  }
+}
+
+void TiledLoader::addTileSet(const std::string &name, size_t first,
+                             UsedSets &used) {
+  /* Load tileset if not loaded */
+  auto i = tileSets.lower_bound(name);
+  if (i == tileSets.end() || i->first != name) {
+    i = tileSets.insert(i, make_pair(name, loadFromTSX(name, assets)));
+    auto &image = i->second.filename;
+    /* Load tile image if not loaded */
+    auto j = textures.try_emplace(image);
+    if (j.second) {
+      auto textureData = assets.getData(image);
+      if (!j.first->second.loadFromMemory(textureData->data,
+                                          textureData->size)) {
+        throw runtime_error("Probably data corruption");
+      }
+    }
+  }
+  used.insert(make_pair(first, i->second));
+}
+
+TiledMap::TiledMap(std::vector<TiledLayer> &&layers) : layers(layers) {}
+void TiledMap::draw(sf::RenderTarget &target, const sf::RenderStates &states) const {
+  RenderStates mine(states);
+  mine.transform *= getTransform();
+  for (auto &layer : layers) {
+    target.draw(layer, mine);
+  }
+}
